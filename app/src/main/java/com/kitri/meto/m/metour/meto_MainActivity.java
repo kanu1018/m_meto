abc074e6b6f038bcd866108e9e4b4b3de2aaf22d
kanu <rlaeodid1018@naver.com>
2016-08-08 11:21:30 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/kitri/meto/m/metour/meto_MainActivity.java
@@ -0,0 +1,13 @@
+package com.kitri.meto.m.metour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class meto_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
